47a79b03bbfdedb64721486b8035548afa9bbbf2
Bae JiHyeon <miel0806@tukorea.ac.kr>
2022-10-18 17:03:35 +0900

    과제 6.1 (Server) 수정

--- TCPServer_FixedVariable.cpp
+++ TCPServer_FixedVariable.cpp
@@ -3,112 +3,138 @@
 #define SERVERPORT 9000
 #define BUFSIZE    1024
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	int retval;
+    int retval;
 
-	// 윈속 초기화
-	WSADATA wsa;
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-		return 1;
+    // 윈속 초기화
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+        return 1;
 
-	// 소켓 생성
-	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (listen_sock == INVALID_SOCKET) err_quit("socket()");
+    // 소켓 생성
+    SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_sock == INVALID_SOCKET) err_quit("socket()");
 
-	// bind()
-	struct sockaddr_in serveraddr;
-	memset(&serveraddr, 0, sizeof(serveraddr));
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serveraddr.sin_port = htons(SERVERPORT);
-	retval = bind(listen_sock, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
-	if (retval == SOCKET_ERROR) err_quit("bind()");
+    // bind()
+    struct sockaddr_in serveraddr;
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons(SERVERPORT);
+    retval = bind(listen_sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+    if (retval == SOCKET_ERROR) err_quit("bind()");
 
-	// listen()
-	retval = listen(listen_sock, SOMAXCONN);
-	if (retval == SOCKET_ERROR) err_quit("listen()");
+    // listen()
+    retval = listen(listen_sock, SOMAXCONN);
+    if (retval == SOCKET_ERROR) err_quit("listen()");
 
-	// 데이터 통신에 사용할 변수
-	SOCKET client_sock;
-	struct sockaddr_in clientaddr;
-	int addrlen;
-	char buf[BUFSIZE]; // 가변 길이 데이터
+    // 데이터 통신에 사용할 변수
+    SOCKET client_sock;
+    struct sockaddr_in clientaddr;
+    int addrlen;
+    //int len; // 고정 길이 데이터
+    char buf[BUFSIZE]; // 가변 길이 데이터
 
-	while (1) {
-		// accept()
-		addrlen = sizeof(clientaddr);
-		client_sock = accept(listen_sock, (struct sockaddr *)&clientaddr, &addrlen);
-		if (client_sock == INVALID_SOCKET) {
-			err_display("accept()");
-			break;
-		}
+    while (1) {
+        // accept()
+        addrlen = sizeof(clientaddr);
+        client_sock = accept(listen_sock, (struct sockaddr*)&clientaddr, &addrlen);
+        if (client_sock == INVALID_SOCKET) {
+            err_display("accept()");
+            break;
+        }
 
-		// 접속한 클라이언트 정보 출력
-		char addr[INET_ADDRSTRLEN];
-		inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
-		printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
-			addr, ntohs(clientaddr.sin_port));
+        // 접속한 클라이언트 정보 출력
+        char addr[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+        printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
+            addr, ntohs(clientaddr.sin_port));
 
-		char file_name[256];
-		memset(&file_name, 0, sizeof(file_name));
-		retval = recv(client_sock, file_name, sizeof(file_name), 0);
-		if (retval == SOCKET_ERROR) {
-			err_display("recv()");
-			break;
-		}
+        char file_name[BUFSIZE];
+        memset(&file_name, 0, sizeof(file_name));
+        retval = recv(client_sock, file_name, sizeof(file_name), 0);
+        if (retval == SOCKET_ERROR) {
+            err_display("recv()");
+            break;
+        }
 
-		printf("전송 받을 파일명: %s\n", file_name);
+        printf("전송 받을 파일명: %s\n", file_name);
 
-		long long file_size = 0;
-		retval = recv(client_sock, (char*)&file_size, sizeof(file_size), 0);
-		if (retval == SOCKET_ERROR) {
-			err_display("recv()");
-			break;
-		}
+        long long file_size = 0;
+        retval = recv(client_sock, (char*)&file_size, sizeof(file_size), 0);
+        if (retval == SOCKET_ERROR) {
+            err_display("recv()");
+            break;
+        }
 
-		FILE* file = fopen(file_name, "wb");
-		long long size = 0;
-		long long last = 0;
+        FILE* file = fopen(file_name, "wb");
+        long long size = 0;
+        long long last = 0;
 
-		while (1) {
-			retval = recv(client_sock, buf, BUFSIZE, 0);
+        while (1) {
+            retval = recv(client_sock, buf, BUFSIZE, 0);
 
-			if (retval == SOCKET_ERROR) {
-				err_display("recv()");
-				break;
-			}
+            if (retval == SOCKET_ERROR) {
+                err_display("recv()");
+                break;
+            }
 
-			else if (size == file_size) {
-				printf("\n파일 전송을 완료했습니다.\n");
+            else if (size == file_size) {
+                printf("\n파일 전송을 완료했습니다.\n");
 
-				fclose(file);
-				break;
-			}
+                fclose(file);
+                break;
+            }
 
-			else {
-				fwrite(buf, 1, retval, file);
+            else {
+                fwrite(buf, 1, retval, file);
 
-				size += retval;
+                size += retval;
 
-				if (last != (float)size / file_size * 100) {
-					printf("\r%.1f%% 전송...", (float)size / file_size * 100);
-				}
+                if (last != (float)size / file_size * 100) {
+                    printf("\r%.1f%% 전송...", (float)size / file_size * 100);
+                }
 
-				last = (float)size / file_size * 100;
-			}
-		}
+                last = (float)size / file_size * 100;
+            }
+        }
 
-		// 소켓 닫기
-		closesocket(client_sock);
-		printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\n",
-			addr, ntohs(clientaddr.sin_port));
-	}
+        //// 클라이언트와 데이터 통신
+        //while (1) {
+        //   // 데이터 받기(고정 길이)
+        //   retval = recv(client_sock, (char *)&len, sizeof(int), MSG_WAITALL);
+        //   if (retval == SOCKET_ERROR) {
+        //      err_display("recv()");
+        //      break;
+        //   }
+        //   else if (retval == 0)
+        //      break;
 
-	// 소켓 닫기
-	closesocket(listen_sock);
+        //   // 데이터 받기(가변 길이)
+        //   retval = recv(client_sock, buf, len, MSG_WAITALL);
+        //   if (retval == SOCKET_ERROR) {
+        //      err_display("recv()");
+        //      break;
+        //   }
+        //   else if (retval == 0)
+        //      break;
 
-	// 윈속 종료
-	WSACleanup();
-	return 0;
+        //   // 받은 데이터 출력
+        //   buf[retval] = '\0';
+        //   printf("[TCP/%s:%d] %s\n", addr, ntohs(clientaddr.sin_port), buf);
+        //}
+
+        // 소켓 닫기
+        closesocket(client_sock);
+        printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\n",
+            addr, ntohs(clientaddr.sin_port));
+    }
+
+    // 소켓 닫기
+    closesocket(listen_sock);
+
+    // 윈속 종료
+    WSACleanup();
+    return 0;
 }
